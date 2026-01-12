9ba41420010568ed7d719f04c15cf44a83f2017a
jasleeeen <jasleen7904@gmail.com>
2026-01-12 14:57:49 +0530

    Day 5 - Greeting

---
+++ Day 5 - GreetingApp/GreetingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            String name = Console.ReadLine();
+            String greetingMessage = GreetingHelper.GetGreeting(name);
+            Console.WriteLine(greetingMessage);
+
+        }
+    }
+}
