b5c867ed31d06caab127b182444ccb94736882fc
jasleeeen <jasleen7904@gmail.com>
2026-01-21 15:06:27 +0530

    Create Day 13 - Print Characters.cs

---
+++ Day 13 - Print Characters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppDashes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DrawLine();
+            DrawLine('+');
+            DrawLine('$', 60);
+        }
+        static void DrawLine(char symbol = '-', int? number = null)
+        {
+            if (number != null)
+            {
+                Console.WriteLine(number);
+            }
+
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+}
