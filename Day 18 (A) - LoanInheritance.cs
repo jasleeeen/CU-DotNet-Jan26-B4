cf04d115746d0ff424523e42e3b30d9304d73134
jasleeeen <jasleen7904@gmail.com>
2026-01-27 15:15:12 +0530

    Day 18

---
+++ Day 18 (A) - LoanInheritance.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace ConsoleApp27._1
+{
+    internal class Loan
+    {
+        public string LoanNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal PrincipalAmount { get; set; }
+        public int TenureInYears { get; set; }
+        public Loan(string loanNum, string custName, decimal prinAmount, int tenure)
+        {
+            LoanNumber = loanNum;
+            CustomerName = custName;
+            PrincipalAmount = prinAmount;
+            TenureInYears = tenure;
+            Console.WriteLine("Loan constructor called");
+        }
+        public static decimal CalculateEMI(decimal prinAmount, int tenure)
+        {
+            decimal totalAmount = prinAmount + (prinAmount * 0.10m * tenure);
+            decimal emi = totalAmount / (tenure * 12);
+            Console.WriteLine("Base class called");
+            return emi;
+        }
+    }
+    internal class HomeLoan : Loan
+    {
+        public HomeLoan(string loanNum, string custName, decimal prinAmount, int tenure)
+            : base(loanNum, custName, prinAmount, tenure)
+        {
+            Console.WriteLine("HomeLoan constructor called");
+        }
+        public static new decimal CalculateEMI(decimal prinAmount, int tenure)
+        {
+            decimal processingFee = prinAmount * 0.01m;
+            prinAmount += processingFee;
+
+            decimal totalAmount = prinAmount + (prinAmount * 0.08m * tenure);
+            decimal emi = totalAmount / (tenure * 12);
+            Console.WriteLine("HomeLoan class called");
+            return emi;
+        }
+    }
+    internal class CarLoan : Loan
+    {
+        public CarLoan(string loanNum, string custName, decimal prinAmount, int tenure)
+            : base(loanNum, custName, prinAmount, tenure)
+        {
+            Console.WriteLine("CarLoan constructor called");
+        }
+        public static new decimal CalculateEMI(decimal prinAmount, int tenure)
+        {
+            prinAmount += 15000;
+            decimal totalAmount = prinAmount + (prinAmount * 0.09m * tenure);
+            decimal emi = totalAmount / (tenure * 12);
+            Console.WriteLine("CarLoan class called");
+            return emi;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loan[] loans = new Loan[4];
+            loans[0] = new HomeLoan("H1", "Alice", 1000000, 15);
+            loans[1] = new HomeLoan("H2", "Alicey", 1500000, 10);
+            loans[2] = new CarLoan("C1", "Bob", 500000, 5);
+            loans[3] = new CarLoan("C2", "Bobby", 780000, 7);
+            for (int i = 0; i < loans.Length; i++)
+            {
+                decimal emi = Loan.CalculateEMI(loans[i].PrincipalAmount, loans[i].TenureInYears);
+                Console.WriteLine($"EMI: {emi:F2}");
+            }
+        }
+    }
+}
---
+++ Day 18 (B) - Employee Compensation Management System.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27._1
+{
+    internal class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal BasicSalary { get; set; }
+        public int ExperienceInYears { get; set; }
+        public Employee(int empID, string empName, decimal basicSalary, int expInYears)
+        {
+            EmployeeID = empID;
+            EmployeeName = empName;
+            BasicSalary = basicSalary;
+            ExperienceInYears = expInYears;
+        }
+        public decimal CalculateAnnualSalary()
+        {
+            decimal AnnualSalary = BasicSalary * 12;
+            return AnnualSalary;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"Employee ID : {EmployeeID}, Employee Name : {EmployeeName}, Experience In Years : {ExperienceInYears} , Annual Salary : {CalculateAnnualSalary()}");
+        }
+    }
+
+
+    internal class PermanentEmployee : Employee
+    {
+        public PermanentEmployee(int empID, string empName, decimal basicSalary, int expInYears) : base(empID, empName, basicSalary, expInYears)
+        {
+            Console.WriteLine("PermanentEmployee constructor called");
+        }
+        public new decimal CalculateAnnualSalary()
+        {
+            decimal houseRentAllowance = 0.2m * BasicSalary;
+            decimal specialAllowance = 0.1m * BasicSalary;
+            decimal loyaltyBonus = 0;
+            if (ExperienceInYears >= 5) loyaltyBonus = 50000m;
+            decimal AnnualSalary = ((BasicSalary + houseRentAllowance + specialAllowance)*12) + loyaltyBonus;
+            return AnnualSalary;
+        }
+        public new void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"Employee ID : {EmployeeID}, Employee Name : {EmployeeName}, Experience In Years : {ExperienceInYears} , Annual Salary : {CalculateAnnualSalary()}");
+        }
+    }
+
+
+    internal class ContractEmployee : Employee
+    {
+        public int ContractDurationInMonths { get; set; }
+        public ContractEmployee(int empID, string empName, decimal basicSalary, int expInYears, int contractDuration) : base(empID, empName, basicSalary, expInYears)
+        {
+            ContractDurationInMonths = contractDuration;
+            Console.WriteLine("ContractEmployee constructor called");
+        }
+        public new decimal CalculateAnnualSalary()
+        {
+            int contractCompletionBonus = 0;
+            if (ContractDurationInMonths >= 12) contractCompletionBonus = 30000;
+            decimal AnnualSalary = (BasicSalary * 12) + contractCompletionBonus;
+            return AnnualSalary;
+        }
+        public new void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"Employee ID : {EmployeeID}, Employee Name : {EmployeeName}, Experience In Years : {ExperienceInYears} , Annual Salary : {CalculateAnnualSalary()}, Contract Duration : {ContractDurationInMonths}");
+        }
+    }
+
+
+    internal class InternEmployee : Employee
+    {
+        public InternEmployee(int empID, string empName, decimal stipend, int expInYears) : base(empID, empName, stipend, expInYears)
+        {
+            Console.WriteLine("InternEmployee constructor called");
+        }
+        public new decimal CalculateAnnualSalary()
+        {
+            decimal AnnualSalary = BasicSalary * 12;
+            return AnnualSalary;
+        }
+        public new void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"Employee ID : {EmployeeID}, Employee Name : {EmployeeName}, Experience In Years : {ExperienceInYears} , Annual Salary/Stipend : {CalculateAnnualSalary()}");
+        }
+    }
+
+
+    internal class Employee_Compensation_Management_System
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(1, "A", 500000, 2);
+            Employee e2 = new PermanentEmployee(2, "B", 500000, 2);
+            PermanentEmployee e3 = new PermanentEmployee(3, "C", 700000, 3);
+            ContractEmployee e4 = new ContractEmployee(4, "D", 60000, 2, 14);
+            InternEmployee e5 = new InternEmployee(5, "E", 25000, 0);
+            e1.DisplayEmployeeDetails();
+            e2.DisplayEmployeeDetails();
+            e3.DisplayEmployeeDetails();
+            e4.DisplayEmployeeDetails();
+            e5.DisplayEmployeeDetails();
+        }
+
+    }
+}
