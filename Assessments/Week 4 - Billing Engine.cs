3396db739f139b59b5d6ad9bae4a043f9cbe13c6
jasleeeen <130594699+jasleeeen@users.noreply.github.com>
2026-01-31 12:29:29 +0530

    Fix condition for valid SeverityLevel check

--- Assessments/Week 4 - Billing Engine.cs
+++ Assessments/Week 4 - Billing Engine.cs
@@ -49,7 +49,7 @@ namespace Assessment4Billing_Engine
         public int SeverityLevel { get; set; }
         override public decimal CalculateFinalBill()
         {
-            if (SeverityLevel < 0 && SeverityLevel > 5) {
+            if (SeverityLevel < 0 || SeverityLevel > 5) {
                 Console.WriteLine("Invalid Severity Level");
                 return BaseFee;
             }
