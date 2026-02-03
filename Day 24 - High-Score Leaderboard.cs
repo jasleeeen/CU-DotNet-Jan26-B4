c8e2b2c7b8bde6666872d64db84a996362a70d68
jasleeeen <jasleen7904@gmail.com>
2026-02-03 14:32:08 +0530

    Day 24 - 1,2

---
+++ Day 24 - High-Score Leaderboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3._2Ex
+{
+    internal class High_Score_Leaderboard
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderboard = new SortedDictionary<double, string>();
+            leaderboard.Add(55.42, "SwiftRacer");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+            Console.WriteLine("Leaderboard ");
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine($"Lap Time: {item.Key,-5:F2}, Name: {item.Value}");
+            }
+            var fastest = leaderboard.First();
+            Console.WriteLine($"\nFastest Lap Time: \nName: {fastest.Value}\nTime: {fastest.Key:F2}\n");
+            Console.WriteLine("SteadyEddie's improved time: 54.00\n");
+            leaderboard.Remove(58.91);
+            leaderboard.Add(54.00, "SteadyEddie");
+            Console.WriteLine("Leaderboard ");
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine($"Lap Time: {item.Key, -5:F2}, {"Name"}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ Day 24 - Legacy Employee Directory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace ConsoleApp3._2Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable employeeTable = new Hashtable();
+            employeeTable.Add(101, "Alice");
+            employeeTable.Add(102, "Bob");
+            employeeTable.Add(103, "Charlie");
+            employeeTable.Add(104, "Diana");
+            if(!employeeTable.ContainsKey(105)) employeeTable.Add(105, "Edward");
+            else Console.WriteLine("ID already exists.");
+            string name = (string)employeeTable[102];
+            Console.WriteLine($"Employee Name of Employee with ID 102 : {name}.\n");
+            Console.WriteLine("Employee Table");
+            foreach (DictionaryEntry item in employeeTable)
+            {
+                Console.WriteLine($"ID: {item.Key}, Name: {item.Value}");
+
+            }
+            employeeTable.Remove(103);
+            Console.WriteLine("\nEmployee with ID 103 removed.\n");
+            Console.WriteLine($"Employee Count: {employeeTable.Count}\n");
+
+            foreach (DictionaryEntry item in employeeTable)
+            {
+                Console.WriteLine($"ID: {item.Key}, Name: {item.Value}");
+
+            }
+        }
+    }
+}
