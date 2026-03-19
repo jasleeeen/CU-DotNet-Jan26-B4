08957587303e5982ea4f602fe2bc9468087d1ff4
jasleeeen <jasleen7904@gmail.com>
2026-03-19 09:39:07 +0530

    Day 60

---
+++ Exercises/Day 60/WebApplicationAssets/ViewModels/InvestmentCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationAssets.ViewModels
+{
+    public class InvestmentCreateViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Asset Name")]
+        public string AssetName { get; set; }
+
+        [Required(ErrorMessage = "Ticker is required")]
+        [StringLength(10)]
+        [Display(Name = "Ticker Symbol")]
+        public string TickerSymbol { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Total Investment Value")]
+        public string TotalValue => (Price * Quantity).ToString("C");
+
+    }
+}
