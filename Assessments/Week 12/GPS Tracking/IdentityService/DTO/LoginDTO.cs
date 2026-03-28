7054f8bd1b2368454a1a71c437cfa415479730d5
jasleeeen <jasleen7904@gmail.com>
2026-03-28 11:57:56 +0530

    Week 12 Assessment

---
+++ Assessments/Week 12/GPS Tracking/IdentityService/DTO/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityService.DTO
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Assessments/Week 12/GPS Tracking/TrackingService/Model/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace TrackingService.Model
+{
+    public class Location
+    {
+        public string TruckId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
