c602772dc9e7bb443dc8d4696f8e6f7b1663e63a
jasleeeen <jasleen7904@gmail.com>
2026-03-31 11:53:07 +0530

    Organise

---
+++ Assessments/Week 1 - GreetingApp/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrEmpty(name)) { return "Hello, Guest!"; }
+            else { return $"Hello, {name}!"; }
+        }
+    }
+}
